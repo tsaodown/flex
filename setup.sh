4925dea8d4ddfb9873b003381cef60017e052828
Edd Tsao <etphonehomies@gmail.com>
2019-08-05 17:17:45 -0700

    Cleanup setup script output

--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-git clone git@github.com:tsaodown/flex.git /usr/local/share/flex
+die() {
+  if [[ ! -z "$*" ]]; then echo "$*" 1>&2; fi
+  printf "\e[91m$(cat /tmp/flex-setup)\e[0m\n"
+  rm /tmp/flex-setup
+  exit 1
+}
+checked() {
+  printf "\e[1;92m$1\e[0m\n"
+  eval "${*:2}" > /tmp/flex-setup 2>&1 || die
+}
+
+checked 'Cloning flex tool...' git clone git@github.com:tsaodown/flex.git /usr/local/share/flex
 cwd=$(pwd)
 cd /usr/local/share/flex/setup_scripts
 for s in *.sh; do
-  /bin/bash "$f" -H || break
+  checked "Running $s..." /bin/bash "$s" -H || break
 done
 cd $cwd
